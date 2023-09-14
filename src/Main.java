0d5e07b0efc6392afb2434845d36d20fa1752ce2
Hello world <Opokukka@mail.uc.edu>
2023-09-14 16:16:04 -0400

    salesTax

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main
-{
-    public static void main(String[] args) {
+{   public static void main(String[] args)
+    { double purchasePrice = 20;
+      double salesTax = 0.05;
+      double totalPrice = 0;
+
+      totalPrice = salesTax * purchasePrice + purchasePrice;
+
+
+      System.out.println("The purchase price is " + purchasePrice);
+      System.out.println("You will have a 5% sales tax");
+
+      System.out.println("Your total price is " + totalPrice );
 
 
     }
